94f39463f68d1ee4c611d1ba5000109b66e3a154
Aastha Neupane <aastha.pane12@gmail.com>
2021-08-24 17:24:57 -0400

    initial commit

---
+++ src/main/java/com/project/spring/ProjectSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringJavaApplication.class, args);
+	}
+
+}
